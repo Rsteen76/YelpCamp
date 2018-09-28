1996f5158bccc584acad231793230d98d6918408
Ryan Steen <ryansteen76@gmail.com>
2018-09-27 20:58:12 -0700

    Style changes

--- app.js
+++ app.js
@@ -57,6 +57,9 @@ app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function () {
+app.listen(3000, process.env.IP, function () {
     console.log('Yelp Camp server is running');
 });
+// app.listen(process.env.PORT, process.env.IP, function () {
+//     console.log('Yelp Camp server is running');
+// });
